b7765335090799e81641c57b6d1a7d88f7c831de
unknown <venta.mexy@gmail.com>
2022-04-18 13:09:33 -0500

    Avance galeria

---
+++ assets/js/iniciarlizar-filterizr.js
@@ -0,0 +1,47 @@
+// Incializando filterizr
+// Default options
+let filter = $(".filter-container").filterizr();
+let options = {
+  animationDuration: 0.5, // in seconds
+  controlsSelector: '', // Selector for custom controls
+  delay: 0, // Transition delay in ms
+  delayMode: 'progressive', // 'progressive' or 'alternate'
+  easing: 'ease-out',
+  filter: 'all', // Initial filter
+  filterOutCss: { // Filtering out animation
+    opacity: 1,
+    transform: 'scale(0.5)'
+  },
+  filterInCss: { // Filtering in animation
+    opacity: 1,
+    transform: 'scale(1)'
+  },
+  gridItemsSelector: '.filter-container',
+  gutterPixels: 0, // Items spacing in pixels
+  layout: 'sameSize', // See layouts
+  multifilterLogicalOperator: 'or',
+  searchTerm: '',
+  setupControls: true, // Should be false if controlsSelector is set 
+  spinner: { // Configuration for built-in spinner
+    enabled: false,
+    fillColor: '#2184D0',
+    styles: {
+      height: '75px',
+      margin: '0 auto',
+      width: '75px',
+      'z-index': 2,
+    },
+  },
+}
+
+filter.filterizr('setOptions', options);
+
+$(document).on("mouseover", ".filtr-item", function() {
+  let barraImagen = $(this)[0].childNodes[3];
+  barraImagen.classList.add("barra-baja-activa");
+});
+
+$(document).on("mouseout", ".filtr-item", function() {
+  let barraImagen = $(".filtr-item .barra-baja");
+  barraImagen.removeClass("barra-baja-activa").addClass("barra-baja");
+});
