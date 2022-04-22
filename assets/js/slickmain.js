d66843177c7a116a7563b360c1909ea8bd932704
unknown <venta.mexy@gmail.com>
2022-04-22 16:21:53 -0500

    Se agregó slider de clientes.

---
+++ assets/js/slickmain.js
@@ -0,0 +1,10 @@
+$('.multiple-items').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    autoplay:false,
+    autoplaySpeed: 2000,
+    arrows:false,
+    slidesToShow: 3,
+    slidesToScroll: 1
+ });
